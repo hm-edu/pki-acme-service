75dc358faf1af3ec185194355c6f5e5db0bc2bd0
Florian Ritterhoff <florian@ritterhoff.org>
2022-10-22 08:30:13 +0200

    Adaptions for MUAS
    - Add Connection to EAB Management Service
    - Split Endpoints
    - Passthrough Context for Actions
    - Add Sectigo/PKI Client
    - Add Jaeger Telemetry
    - Fix minor issues handling EAB

--- cmd/step-ca/main.go
+++ cmd/step-ca/main.go
@@ -37,6 +37,7 @@ import (
 
 	// Enabled cas interfaces.
 	_ "github.com/smallstep/certificates/cas/cloudcas"
+	_ "github.com/smallstep/certificates/cas/sectigocas"
 	_ "github.com/smallstep/certificates/cas/softcas"
 	_ "github.com/smallstep/certificates/cas/stepcas"
 	_ "github.com/smallstep/certificates/cas/vaultcas"
