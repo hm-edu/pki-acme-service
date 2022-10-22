b8dd33fef97e46c4bd5a46dcdf9e3d015fe68c00
Florian Ritterhoff <florian@ritterhoff.org>
2022-10-22 08:30:13 +0200

    Adaptions for MUAS
    - Add Connection to EAB Management Service
    - Split Endpoints
    - Passthrough Context for Actions
    - Add Sectigo/PKI Client
    - Add Jaeger Telemetry
    - Fix minor issues handling EAB

--- cas/stepcas/stepcas.go
+++ cas/stepcas/stepcas.go
@@ -72,7 +72,7 @@ func (s *StepCAS) Type() apiv1.Type {
 
 // CreateCertificate uses the step-ca sign request with the configured
 // provisioner to get a new certificate from the certificate authority.
-func (s *StepCAS) CreateCertificate(req *apiv1.CreateCertificateRequest) (*apiv1.CreateCertificateResponse, error) {
+func (s *StepCAS) CreateCertificate(ctx context.Context, req *apiv1.CreateCertificateRequest) (*apiv1.CreateCertificateResponse, error) {
 	switch {
 	case req.CSR == nil:
 		return nil, errors.New("createCertificateRequest `csr` cannot be nil")
@@ -107,7 +107,7 @@ func (s *StepCAS) CreateCertificate(req *apiv1.CreateCertificateRequest) (*apiv1
 
 // RenewCertificate will always return a non-implemented error as mTLS renewals
 // are not supported yet.
-func (s *StepCAS) RenewCertificate(req *apiv1.RenewCertificateRequest) (*apiv1.RenewCertificateResponse, error) {
+func (s *StepCAS) RenewCertificate(ctx context.Context, req *apiv1.RenewCertificateRequest) (*apiv1.RenewCertificateResponse, error) {
 	if req.Token == "" {
 		return nil, apiv1.ValidationError{Message: "renewCertificateRequest `token` cannot be empty"}
 	}
@@ -130,7 +130,7 @@ func (s *StepCAS) RenewCertificate(req *apiv1.RenewCertificateRequest) (*apiv1.R
 }
 
 // RevokeCertificate revokes a certificate.
-func (s *StepCAS) RevokeCertificate(req *apiv1.RevokeCertificateRequest) (*apiv1.RevokeCertificateResponse, error) {
+func (s *StepCAS) RevokeCertificate(ctx context.Context, req *apiv1.RevokeCertificateRequest) (*apiv1.RevokeCertificateResponse, error) {
 	if req.SerialNumber == "" && req.Certificate == nil {
 		return nil, errors.New("revokeCertificateRequest `serialNumber` or `certificate` are required")
 	}
