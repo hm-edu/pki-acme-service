99c3d1f900209f2afcae494ba12aff69b95f07c9
Florian Ritterhoff <florian@ritterhoff.org>
2022-10-22 08:30:13 +0200

    Adaptions for MUAS
    - Add Connection to EAB Management Service
    - Split Endpoints
    - Passthrough Context for Actions
    - Add Sectigo/PKI Client
    - Add Jaeger Telemetry
    - Fix minor issues handling EAB

--- cas/cas_test.go
+++ cas/cas_test.go
@@ -18,15 +18,15 @@ import (
 
 type mockCAS struct{}
 
-func (m *mockCAS) CreateCertificate(*apiv1.CreateCertificateRequest) (*apiv1.CreateCertificateResponse, error) {
+func (m *mockCAS) CreateCertificate(ctx context.Context, req *apiv1.CreateCertificateRequest) (*apiv1.CreateCertificateResponse, error) {
 	panic("not implemented")
 }
 
-func (m *mockCAS) RenewCertificate(*apiv1.RenewCertificateRequest) (*apiv1.RenewCertificateResponse, error) {
+func (m *mockCAS) RenewCertificate(ctx context.Context, req *apiv1.RenewCertificateRequest) (*apiv1.RenewCertificateResponse, error) {
 	panic("not implemented")
 }
 
-func (m *mockCAS) RevokeCertificate(*apiv1.RevokeCertificateRequest) (*apiv1.RevokeCertificateResponse, error) {
+func (m *mockCAS) RevokeCertificate(ctx context.Context, req *apiv1.RevokeCertificateRequest) (*apiv1.RevokeCertificateResponse, error) {
 	panic("not implemented")
 }
 
