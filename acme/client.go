7e02a1091938a39e37b32d92a59152d40cb4196d
Florian Ritterhoff <florian@ritterhoff.org>
2024-08-08 17:32:57 +0200

    feat: custom dns resolver

--- acme/client.go
+++ acme/client.go
@@ -3,9 +3,14 @@ package acme
 import (
 	"context"
 	"crypto/tls"
+	"fmt"
 	"net"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/miekg/dns"
+	"github.com/sirupsen/logrus"
 )
 
 // Client is the interface used to verify ACME challenges.
@@ -72,8 +77,102 @@ func (c *client) Get(url string) (*http.Response, error) {
 	return c.http.Get(url)
 }
 
+var timeouts [5]time.Duration = [5]time.Duration{(time.Second * 1), (time.Second * 1), (time.Second * 2), (time.Second * 4), (time.Second * 2)}
+
+func ResolveWithTimeout(name, resolver string, qtype, qclass uint16) (*dns.Msg, error) {
+	client := new(dns.Client)
+	msg := &dns.Msg{
+		MsgHdr: dns.MsgHdr{
+			Id:               dns.Id(),
+			RecursionDesired: true,
+		},
+		Question: []dns.Question{{Name: dns.Fqdn(name), Qtype: qtype, Qclass: qclass}},
+	}
+	msg.AuthenticatedData = true
+	msg.SetEdns0(4096, true)
+
+	for i := 0; i < len(timeouts); i++ {
+
+		client.Timeout = timeouts[i]
+		resp, _, err := client.Exchange(msg, fmt.Sprintf("%s:53", resolver))
+		if err == nil && resp.Truncated {
+			tcpConn, _ := dns.Dial("tcp", fmt.Sprintf("%s:53", resolver))
+			resp, _, err = client.ExchangeWithConn(msg, tcpConn)
+		}
+		if err != nil {
+			if err, ok := err.(net.Error); ok && err.Timeout() {
+				logrus.Warnf("Timeout querying %s records '%s' after %v", dns.TypeToString[qtype], name, timeouts[i])
+				continue
+			}
+			return nil, err
+		}
+
+		return resp, nil
+
+	}
+	return nil, &net.DNSError{
+		Name:      name,
+		Err:       "Final timeout.",
+		IsTimeout: true,
+	}
+}
+
+func LookupTxt(name, resolver string) ([]string, error) {
+	logrus.Infof("Using custom resolver %s for lookup of %s", resolver, name)
+	resp, err := ResolveWithTimeout(name, resolver, dns.TypeTXT, dns.ClassINET)
+	if err != nil {
+		logrus.Warnf("Failed to lookup %s: %v", name, err)
+		return nil, err
+	}
+	nsData := []string{}
+	data := []string{}
+
+	// Check if TXT records are present
+	for _, answer := range resp.Answer {
+		if txt, ok := answer.(*dns.TXT); ok {
+			logrus.Infof("Resolved TXT records for %s: %s", name, txt.Txt)
+			data = append(data, txt.Txt...)
+		}
+	}
+	if len(data) > 0 {
+		return data, nil
+	}
+	resp, err = ResolveWithTimeout(name, resolver, dns.TypeCNAME, dns.ClassINET)
+	if err != nil {
+		logrus.Warnf("Failed to lookup %s: %v", name, err)
+		return nil, err
+	}
+	// Check if CNAME records are present
+	for _, answer := range resp.Answer {
+		if cname, ok := answer.(*dns.CNAME); ok {
+			logrus.Infof("Resolved CNAME records for %s: %s", name, cname.Target)
+			// Get NS records for the CNAME
+			resp, err := ResolveWithTimeout(cname.Target, resolver, dns.TypeSOA, dns.ClassINET)
+			if err != nil {
+				logrus.Warnf("Failed to lookup %s: %v", cname.Target, err)
+				continue
+			}
+			for _, answer := range resp.Ns {
+				if ns, ok := answer.(*dns.SOA); ok {
+					logrus.Infof("Resolved NS records for %s: %s", cname.Target, ns.Ns)
+					nsData = append(nsData, ns.Ns)
+				}
+			}
+			for _, ns := range nsData {
+				logrus.Infof("Resolving %s using %s", cname.Target, ns)
+				return LookupTxt(cname.Target, ns)
+			}
+		}
+	}
+
+	return data, nil
+}
 func (c *client) LookupTxt(name string) ([]string, error) {
-	return net.LookupTXT(name)
+	resolver := os.Getenv("DNS_RESOLVER")
+	if resolver != "" {
+		return LookupTxt(name, resolver)
+	}
+	return c.LookupTxt(name)
 }
 
 func (c *client) TLSDial(network, addr string, config *tls.Config) (*tls.Conn, error) {
