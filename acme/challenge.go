ee90e20a3a58892c74878aae2aeffa62fd5cc40c
Florian Ritterhoff <florian@ritterhoff.org>
2024-08-08 17:32:57 +0200

    feat: custom dns resolver

--- acme/challenge.go
+++ acme/challenge.go
@@ -110,7 +110,7 @@ func (ch *Challenge) ToLog() (interface{}, error) {
 // 'validated' attributes are updated.
 func (ch *Challenge) Validate(ctx context.Context, db DB, jwk *jose.JSONWebKey, payload []byte) error {
 	// If already valid or invalid then return without performing validation.
-	if ch.Status != StatusPending {
+	if ch.Status != StatusPending && ch.Status != StatusInvalid {
 		return nil
 	}
 	switch ch.Type {
@@ -405,6 +405,7 @@ func dns01Validate(ctx context.Context, ch *Challenge, db DB, jwk *jose.JSONWebK
 	vc := MustClientFromContext(ctx)
 	txtRecords, err := vc.LookupTxt(dns01ChallengeHost(domain))
 	if err != nil {
+		logrus.Warnf("error looking up TXT records for domain %s: %v", domain, err)
 		return storeError(ctx, db, ch, false, WrapError(ErrorDNSType, err,
 			"error looking up TXT records for domain %s", domain))
 	}
@@ -1678,6 +1679,7 @@ func KeyAuthorization(token string, jwk *jose.JSONWebKey) (string, error) {
 func storeError(ctx context.Context, db DB, ch *Challenge, markInvalid bool, err *Error) error {
 	ch.Error = err
 	if markInvalid {
+		logrus.Warnf("marking challenge %s as invalid: %v", ch.ID, err)
 		ch.Status = StatusInvalid
 	}
 	if err := db.UpdateChallenge(ctx, ch); err != nil {
